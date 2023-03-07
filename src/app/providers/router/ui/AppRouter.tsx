9085c7bd3f28f4f34cbe4c9e0f3e6512b0b3bfff
TTurkevich <tatu_it@internet.ru>
2023-03-07 15:21:12 +0300

    Added Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgets/PageLoader/PageLoader'
 
 const AppRouter = () => (
-  // eslint-disable-next-line i18next/no-literal-string
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<PageLoader />}>
     <Routes>
       {Object.values(routeConfig).map(({ element, path }) => (
         <Route key={path} path={path} element={<div className='page-wrapper'>{element}</div>} />
---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import type { PropsWithChildren } from 'react'
+import { useTranslation } from 'react-i18next'
+import cls from './NotFoundPage.module.scss'
+
+interface NotFoundPageProps {
+  className?: string
+}
+
+export const NotFoundPage = (props: PropsWithChildren<NotFoundPageProps>) => {
+  const { className } = props
+  const { t } = useTranslation()
+
+  return <div className={classNames(cls.NotFoundPage, {}, [className])}>{t('Страница не найдена')}</div>
+}
